ea4807f3ee9c77f6e1142c820fe93a19e2ac5905
Mike Shevchenko <*>
2023-06-15 15:58:11 +0200

    Add retrofit, moshi deserialization support, change Internet permission

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.rickandmorty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        val rickAndMortyService : RickAndMortyService = retrofit.create(RickAndMortyService::class.java)
+        rickAndMortyService.getCharacterById().enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.i("MainActivity",response.toString())
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.i("MainActivity",t.message?: "Null message")
+            }
+
+        })   //using enqueue because calling from main thread ui
     }
 }
